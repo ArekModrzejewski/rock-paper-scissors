b0f57767023f6ebbbc6bdb67ac61de9d13ac978d
ArekModrzejewski <arekmod98@gmail.com>
2021-03-07 13:29:30 +0100

    Added function playRound simulating 1 round of the game

--- script.js
+++ script.js
@@ -11,3 +11,60 @@ function computerPlay() {
         return 'Scissors';
     }
 }
+
+function playRound() {
+    let playerChoice = prompt('Rock , Paper or Scissors?').toLowerCase();
+    let playerChoiceCapitalized = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+    let computerChoice = computerPlay();
+    let result;
+    if (playerChoice == 'rock') {
+        if (computerChoice == 'Scissors') {
+            result = 'player';
+        }
+        else if (computerChoice == 'Paper') {
+            result = 'computer';
+        }
+        else {
+            result = 'draw';
+        }
+
+    }
+    else if (playerChoice == 'paper') {
+        if (computerChoice == 'Rock') {
+            result = 'player';
+        }
+        else if (computerChoice == 'Scissors') {
+            result = 'computer';
+        }
+        else {
+            result = 'draw';
+        }
+    }
+    else if (playerChoice == 'scissors') {
+        if (computerChoice == 'Paper') {
+            result = 'player';
+        }
+        else if (computerChoice == 'Rock') {
+            result = 'computer';
+        }
+        else {
+            result = 'draw';
+        }
+    }
+    else {
+        result = 'invalid';
+    }
+
+    if (result == 'player') {
+        console.log(`You win! ${playerChoiceCapitalized} beats ${computerChoice}.`);
+    }
+    else if (result == 'computer') {
+        console.log(`You loose. :c ${computerChoice} beats ${playerChoiceCapitalized}.`);
+    }
+    else if (result == 'draw') {
+        console.log("It's a draw.")
+    }
+    else {
+        console.log('ERROR:Invalid input    Type: rock, paper or scissors')
+    }
+}
